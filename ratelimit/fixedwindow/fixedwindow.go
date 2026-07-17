055c240f87d734c3fe5cfaa37bd2375cf9a0637a
sanskarpan <sanskarpandey2004@gmail.com>
2026-07-17 12:16:30 +0530

    refactor(module-path): update ratelimit/fixedwindow/fixedwindow.go to new module path

--- ratelimit/fixedwindow/fixedwindow.go
+++ ratelimit/fixedwindow/fixedwindow.go
@@ -25,8 +25,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/sanskarpan/resilience/internal/clock"
-	"github.com/sanskarpan/resilience/ratelimit"
+	"github.com/sanskarpan/Rate-Limiter-Circuit-Breaker/internal/clock"
+	"github.com/sanskarpan/Rate-Limiter-Circuit-Breaker/ratelimit"
 )
 
 const algorithmName = "fixed_window"
