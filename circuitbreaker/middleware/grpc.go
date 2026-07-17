a1c959c47a113891206378b4f1935f993a5fd7df
sanskarpan <sanskarpandey2004@gmail.com>
2026-07-17 12:16:52 +0530

    refactor(module-path): update circuitbreaker/middleware/grpc.go to new module path

--- circuitbreaker/middleware/grpc.go
+++ circuitbreaker/middleware/grpc.go
@@ -9,7 +9,7 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
-	"github.com/sanskarpan/resilience/circuitbreaker"
+	"github.com/sanskarpan/Rate-Limiter-Circuit-Breaker/circuitbreaker"
 )
 
 // CBUnaryServerInterceptor returns a gRPC unary server interceptor that wraps
