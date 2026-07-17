ba065add785f4083850522ef668036bb3f2e534b
sanskarpan <sanskarpandey2004@gmail.com>
2026-07-17 12:06:36 +0530

    feat(ratelimit/tokenbucket): add options.go

---
+++ ratelimit/tokenbucket/options.go
@@ -0,0 +1,21 @@
+package tokenbucket
+
+import (
+	"time"
+
+	"github.com/sanskarpan/resilience/internal/clock"
+)
+
+// Option configures a TokenBucket.
+type Option func(*TokenBucket)
+
+// WithClock sets a custom clock for deterministic testing.
+func WithClock(c clock.Clock) Option {
+	return func(tb *TokenBucket) { tb.clock = c }
+}
+
+// WithIdleCleanup sets the duration after which inactive buckets are evicted.
+// Default is 5 minutes. Set to 0 to disable cleanup.
+func WithIdleCleanup(d time.Duration) Option {
+	return func(tb *TokenBucket) { tb.idleClean = d }
+}
