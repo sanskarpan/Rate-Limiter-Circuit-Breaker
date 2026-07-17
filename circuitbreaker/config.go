d306cd7a3fb778ba442f0ae10c5a91a14f2eb5d7
sanskarpan <sanskarpandey2004@gmail.com>
2026-07-17 12:16:52 +0530

    refactor(module-path): update circuitbreaker/config.go to new module path

--- circuitbreaker/config.go
+++ circuitbreaker/config.go
@@ -3,7 +3,7 @@ package circuitbreaker
 import (
 	"time"
 
-	"github.com/sanskarpan/resilience/internal/clock"
+	"github.com/sanskarpan/Rate-Limiter-Circuit-Breaker/internal/clock"
 )
 
 // Config configures a CircuitBreaker.
