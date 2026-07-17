5aaa1e203c32a6ba11816d9d55cb39356fdda1d9
sanskarpan <sanskarpandey2004@gmail.com>
2026-07-17 12:16:08 +0530

    refactor(module-path): update internal/clock/clock_test.go to new module path

--- internal/clock/clock_test.go
+++ internal/clock/clock_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/sanskarpan/resilience/internal/clock"
+	"github.com/sanskarpan/Rate-Limiter-Circuit-Breaker/internal/clock"
 )
 
 func TestManualClock_Now(t *testing.T) {
