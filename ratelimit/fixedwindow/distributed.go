f43802fc033c7b2056de2f85b131ad1bbbaf9369
sanskarpan <sanskarpandey2004@gmail.com>
2026-07-17 12:16:29 +0530

    refactor(module-path): update ratelimit/fixedwindow/distributed.go to new module path

--- ratelimit/fixedwindow/distributed.go
+++ ratelimit/fixedwindow/distributed.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/sanskarpan/resilience/ratelimit"
-	"github.com/sanskarpan/resilience/ratelimit/store"
+	"github.com/sanskarpan/Rate-Limiter-Circuit-Breaker/ratelimit"
+	"github.com/sanskarpan/Rate-Limiter-Circuit-Breaker/ratelimit/store"
 )
 
 // DistributedFixedWindow is a Redis-backed fixed window counter.
